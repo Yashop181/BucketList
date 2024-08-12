fe74d7a682a5a934c3832fb5bff505b8ea24e6d2
Yash <yashnigam1801@gmail.com>
2024-08-12 22:41:33 +0530

    creating a bucket app

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/bucketlist')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+
+  const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
