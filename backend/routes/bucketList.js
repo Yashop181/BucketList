2841773205db56ea45fc9fc77345afecdbd20f16
Yash <yashnigam1801@gmail.com>
2024-08-15 22:28:05 +0530

    add edit and delete

--- backend/routes/bucketList.js
+++ backend/routes/bucketList.js
@@ -25,8 +25,28 @@ router.post('/',async (req,res)=>{
     }
 })
 
+//update a list item
 
+router.patch('/:id', async(req,res)=>{
+    try {
+        const item = await BucketList.findById(req.params.id);
+        if(req.body.name) item.name  = req.body.name;
+        if(req.body.visited !== undefined) item.visited = req.body.visited;
+        const updatedItem  = await item.save();
+        res.json(updatedItem);
+    } catch (error) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
+//delete a list item
 
-
+router.delete('/:id', async(req,res)=>{
+    try {
+        await BucketList.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Item deleted'})
+    } catch (error) {
+        res.status(500).json({ message: err.message });
+    }
+});
 module.exports = router;
